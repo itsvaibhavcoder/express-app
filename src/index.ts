50199049cd28ce8458ced13609f82795d7380637
Vaibhav <sukalevaibhav53@gmail.com>
2024-08-31 19:41:08 +0530

    [vaibhav]added: modified index file

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,64 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import UserRoutes from './routes/user'; // Notice the capital 'U' to match the class name
+import dotenv from 'dotenv';
+dotenv.config();
+import express, {Application} from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import routes from './routes';
+import Database from './config/database';
+import ErrorHandler from './middlewares/error.middleware';
+import Logger from './config/logger';
+import morgan from 'morgan';
 
-const app = express();
+class App{
+  public app: Application;
+  public host: string | number;
+  public port: string | number;
+  public api_version: string | number;
+  public env: boolean;
+  private db = new Database();
+  private logStream = Logger.logStream;
+  private logger = Logger.logger;
+  public errorHandler = new ErrorHandler();
 
-app.use(express.json());
+  constructor() {
+    this.app = express();
+    this.host = process.env.APP_HOST;
+    this.port = process.env.APP_PORT;
+    this.api_version = process.env.API_VERSION;
 
-// MongoDB Connection
-mongoose.connect('mongodb://127.0.0.1:27017/test', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => console.log('MongoDB connected'))
-  .catch(err => console.log(err));
+    this.initializeMiddleWares();
+    this.initializeRoutes();
+    this.initializeDatabase();
+    this.initializeErrorHandlers();
+    this.startApp();
+  }
+  
+  public initializeMiddleWares(): void {
+    this.app.use(cors());
+    this.app.use(helmet());
+    this.app.use(express.urlencoded({ extended: true }));
+    this.app.use(express.json());
+    this.app.use(morgan('combined', { stream: this.logStream }));
+  }
+  public initializeDatabase(): void {
+    this.db.initializeDatabase();
+  }
+  public initializeRoutes(): void {
+    this.app.use(`/api/${this.api_version}`, routes());
+  }
+  public initializeErrorHandlers(): void {
+    this.app.use(this.errorHandler.appErrorHandler);
+    this.app.use(this.errorHandler.genericErrorHandler);
+    this.app.use(this.errorHandler.notFound);
+  }
+  public startApp(): void {
+    this.app.listen(this.port, () => {
+      this.logger.info(
+        `Server started at ${this.host}:${this.port}/api/${this.api_version}/`
+      );
+    });
+ }
+}
 
-// Instantiate the UserRoutes class and get the router instance
-const userRoutes = new UserRoutes(); 
-app.use('/api/users', userRoutes.getRoutes());
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+const app = new App();
+export default app;
