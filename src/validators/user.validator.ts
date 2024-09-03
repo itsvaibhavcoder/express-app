e6b29f57489be83bcf50887ad6a9f1fd53bd08a5
Vaibhav <sukalevaibhav53@gmail.com>
2024-09-03 15:28:43 +0530

    [vaibhav]added: validator for email and password for forgetting and resetting password

--- src/validators/user.validator.ts
+++ src/validators/user.validator.ts
@@ -29,6 +29,30 @@ class UserValidator {
     }
     next();
   }
+
+  public emailValidate = (req: Request, res: Response, next: NextFunction): void => {
+    const schema = Joi.object({
+      email: Joi.string().email().pattern(/^[^\s@]+@[^\s@]+\.[^\s@]+$/).required(),
+    });
+    const { error } = schema.validate(req.body);
+    if (error) {
+      next(error);
+    }
+    next();
+  };
+
+  public resetPasswordValidate = (req: Request, res: Response, next: NextFunction): void => {
+    const schema = Joi.object({
+      token: Joi.string().required(),
+      newPassword: Joi.string().min(8).pattern(/^\S+$/).required(),
+    });
+    const { error } = schema.validate(req.body);
+    if (error) {
+      next(error);
+    }
+    next();
+  };
+
 }
 
 export default UserValidator;
