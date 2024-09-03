ad65a73b5d3b08b68ca4597dbd69385c2e0002c4
Vaibhav <sukalevaibhav53@gmail.com>
2024-09-03 15:27:54 +0530

    [vaibhav]added: services for forget and reset password

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -4,6 +4,7 @@ import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 dotenv.config();
+
 class UserService {
 
   //create new user
@@ -16,7 +17,7 @@ class UserService {
     const data = await User.create(body);
     return data;
   };
-
+  
   public MatchCredential = async (email: string, password: string): Promise<any> => {
     const user = (await User.findOne({email}).exec());
     if(user){
@@ -34,6 +35,43 @@ class UserService {
     const token = jwt.sign(payload, secretKey, {expiresIn: '1h'});
     return token 
   }
+
+  public generatePasswordResetToken = async (email: string): Promise<string> => {
+    const user = await User.findOne({ email });
+    if (!user) {
+      throw new Error('Email does not exist');
+    }
+
+    // Generate a password reset token (you can use JWT or a simple token generator)
+    const resetToken = jwt.sign({ email: user.email }, process.env.JWT_SECRET_KEY, { expiresIn: '15m' });
+
+    // Save the token to the user document (optional)
+    //user.passwordResetToken = resetToken;
+    await user.save();
+
+    return resetToken;
+  };
+
+  public resetPassword = async (token: string, newPassword: string): Promise<void> => {
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY) as { email: string };
+      const user = await User.findOne({ email: decoded.email });
+      if (!user) {
+        throw new Error('Invalid token or user does not exist');
+      }
+
+      // Hash the new password
+      const hashedPassword = await bcrypt.hash(newPassword, 10);
+      user.password = hashedPassword;
+      //user.passwordResetToken = undefined; // Optionally clear the reset token
+      await user.save();
+    } catch (error) {
+      throw new Error('Invalid or expired token');
+    }
+  };
+
+
+  
 };
 
 export default UserService;
